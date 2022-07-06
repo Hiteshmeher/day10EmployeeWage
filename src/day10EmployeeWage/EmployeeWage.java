ba2cd348608a982c2d089c2999c27eb02840a8fd
Hiteshmeher <meher.hitesh4@gmail.com>
2022-07-06 13:50:50 +0530

    Managing Employee Wage for Multiple companies using Array

--- src/day10EmployeeWage/EmployeeWage.java
+++ src/day10EmployeeWage/EmployeeWage.java
@@ -1,51 +1,84 @@
 package day10EmployeeWage;
 
 public class EmployeeWage {
+	int empPresent = 1;
+	int wagePerHour;
+	int fulldayHour;
+	int partTimeHour;
+	double checkingEmployee;
+	String companyName;
 
-	static final int IsPartTime = 0;
-	static final int IsFulltime = 1;
+// Constructor
+	public EmployeeWage(String comp, int empwa, int fulldayhr, int parthr) {
+		this.companyName = comp;
+		this.wagePerHour = empwa;
+		this.fulldayHour = fulldayhr;
+		this.partTimeHour = parthr;
+		this.checkingEmployee = Math.floor(Math.random() * 10) % 3 + 1;
+	}
 
-	public void Emp(int WagePerHour, int FullDayHour, int PartTimeHour, int WorkingDayInMonth,
-			int WorkingHourPerMonth) {
-		/**
-		 * initializing values
-		 * generating random number 0, 1, 2 
-		 * if o the part time job 
-		 * if 1 then present for job
-		 * if 2 then absent 
-		 * using switch case statement printing employee is present or not or part time 
-		 * 
-		 */
-		int dailyWages = 0;
-		int MonthlyWages = 0;
-		int workingDay = 1;
-		int day = 1;
-		int totalWorkingHoursInMonth = 0;
-		while (workingDay <= WorkingDayInMonth && totalWorkingHoursInMonth <= WorkingHourPerMonth) {
-			int attendance = (int) Math.floor(Math.random() * 10) % 3;
-			switch (attendance) {
-			case IsPartTime:
-				System.out.println("Day " + day + " Half day");
-				dailyWages = WagePerHour * PartTimeHour;
-				System.out.println("Wages of the day " + day + " is " + dailyWages);
-				MonthlyWages = dailyWages + MonthlyWages;
-				totalWorkingHoursInMonth = totalWorkingHoursInMonth + PartTimeHour;
+// Employee Attendence
+	public void present_absent() {
+		System.out.println("<<<< Welcome to Employee Wage Computation >>>>");
+		if (checkingEmployee == empPresent) {
+			System.out.println("Employee is present");
+
+		} else if (checkingEmployee == 2) {
+			System.out.println("Employee is absent");
+		} else {
+			System.out.println("Employee is doing part time");
+		}
+	}
+
+// Calculating Daily wages of Employee	
+	public void daily_wage() {
+		if (checkingEmployee == empPresent) {
+			System.out.println("Employee wage for " + companyName + " for a day is " + wagePerHour * fulldayHour);
+		} else if (checkingEmployee == 2) {
+			System.out.println("Sorry Employee is absent !!!!");
+		} else {
+			System.out.println("Employee is doing part time");
+			System.out.println("Employee wage of " + companyName + " per day is " + wagePerHour * partTimeHour);
+		}
+	}
+
+// Calculating Monthly wages of Employee
+	public void monthly_wage() {
+		int month = 20;
+		switch ((int) checkingEmployee) {
+		case 1:
+			System.out.println("Employee wage of " + companyName + " for the month for full time is "
+					+ wagePerHour * fulldayHour * month);
+			break;
+		case 3:
+			System.out.println("Employee wage of " + companyName + "  for the month for part time is "
+					+ wagePerHour * partTimeHour * month);
+			break;
+		case 2:
+			System.out.println("Sorry Employee is absent !!!!");
+		}
+	}
+
+// Calculating Wages of Employee till 20 days or 100 hours in a month
+	public void wage_till_condition() {
+		int dayCount = 1;
+		int hourCount = 0;
+		while (dayCount != 20 && hourCount != 100) {
+			int checkingEmployee = (int) (Math.floor(Math.random() * 10) % 3 + 1);
+			switch (checkingEmployee) {
+			case 1:
+				dayCount += 1;
+				hourCount += fulldayHour;
 				break;
-			case IsFulltime:
-				System.out.println("Day " + day + " present");
-				dailyWages = WagePerHour * FullDayHour;
-				System.out.println("Wages of the day " + day + " is " + dailyWages);
-				MonthlyWages = dailyWages + MonthlyWages;
-				totalWorkingHoursInMonth = totalWorkingHoursInMonth + FullDayHour;
+			case 3:
+				dayCount += 1;
+				hourCount += partTimeHour;
 				break;
 			default:
-				System.out.println("Day " + day + " Absent");
-				break;
+				continue;
 			}
-			day++;
-			workingDay++;
 		}
-		System.out.println("Total Working Hours of the month: " + totalWorkingHoursInMonth);
-		System.out.println("Wages of the month: " + MonthlyWages);
+		System.out.println(
+				"Employee wage of " + companyName + " with condition is " + wagePerHour * hourCount * dayCount);
 	}
 }
